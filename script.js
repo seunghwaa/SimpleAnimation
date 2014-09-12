0bf2e8ed4cd7b217b679d6a6be6769b3f438f5df
seunghwaa <hwa9272@gmail.com>
2014-09-12 15:14:36 +0900

    Tractor.png 추가

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
 
 function preload() {
-
+	game.load.image('cow', 'assets/Tractor.png');
 }
 
 function create() {
-	
+	game.add.sprite(110, 82, 'Tractor');
 }

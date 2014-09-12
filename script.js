9a3192b4c314ae6d8a412d1790716237c6f71c14
seunghwaa <hwa9272@gmail.com>
2014-09-12 15:17:28 +0900

    tractor 수정

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
 
 function preload() {
-	game.load.image('cow', 'assets/Tractor.png');
+	game.load.image('cow', 'assets/tractor.png');
 }
 
 function create() {
-	game.add.sprite(110, 82, 'Tractor');
+	game.add.sprite(110, 82, 'tractor');
 }

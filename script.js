5600b87332653ac712cb37e532594c377d915371
seunghwaa <hwa9272@gmail.com>
2014-09-12 15:42:36 +0900

    cow움직임주기

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
+var cow;
 
 function preload() {
-	game.load.image('cow', 'assets/cow.png');
+	game.load.spritesheet('cow', 'assets/cow.png', 100/*whith*/,50/*height*/);
 }
 
 function create() {
-	game.add.sprite(100, 100, 'cow');
+	cow = game.add.sprite(100, 100, 'cow');
+	cow.animations.add('walk');
+	cow.inputEnabled = true;
+	cow.events.onInputDown.add(cowClick, this);
+
+
+	//cow.animations.play('walk', 10/*속도조절*/, true); 항상 움직임	
+
+}
+
+function cowClick() {
+	cow.animations.play('walk', 10, false);
 }
